1e14c6dc461f8f67fc66ef2657f70f7f85b30f09
Stacey Moore <smoore424@gmail.com>
2019-08-14 17:00:19 -0700

    basic setup of initial view controller and storyViewController

---
+++ CYOA/StoryViewController.swift
@@ -0,0 +1,27 @@
+//
+//  StoryViewController.swift
+//  CYOA
+//
+//  Created by Stacey on 8/14/19.
+//  Copyright © 2019 Stacey. All rights reserved.
+//
+
+import UIKit
+
+class StoryViewController: UIViewController {
+
+    @IBOutlet var storyText: UILabel!
+    
+    @IBOutlet var button1: UIButton!
+    @IBOutlet var button2: UIButton!
+    @IBOutlet var button3: UIButton!
+    @IBOutlet var button4: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
