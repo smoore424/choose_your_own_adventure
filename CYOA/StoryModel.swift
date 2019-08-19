565199623ade062fe6e65061e0f5d12e12330f42
Stacey Moore <smoore424@gmail.com>
2019-08-18 19:17:05 -0700

    added function to buttons, able to move between pages

---
+++ CYOA/StoryModel.swift
@@ -0,0 +1,40 @@
+//
+//  storyModel.swift
+//  CYOA
+//
+//  Created by Stacey on 8/14/19.
+//  Copyright © 2019 Stacey. All rights reserved.
+//
+
+import UIKit
+
+class StoryModel {
+    
+    //id = page number
+    var id: Int
+    //text is the story
+    var text: String
+    //button text is the choice player will make
+    var button1Text: String
+    //the number associated with the button action is the next page that will present.
+    var button1Action: Int
+    var button2Text: String
+    var button2Action: Int
+    var button3Text: String
+    var button3Action: Int
+    var button4Text: String
+    var button4Action: Int
+    
+    init(id: Int, text: String, button1Text: String, button1action: Int, button2text: String, button2Action: Int, button3Text: String, button3Action: Int, button4Text: String, button4Action: Int) {
+        self.id = id
+        self.text = text
+        self.button1Text = button1Text
+        self.button1Action = button1action
+        self.button2Text = button2text
+        self.button2Action = button2Action
+        self.button3Text = button3Text
+        self.button3Action = button3Action
+        self.button4Text = button4Text
+        self.button4Action = button4Action
+    }
+}
--- CYOA/StoryViewController.swift
+++ CYOA/StoryViewController.swift
@@ -17,10 +17,36 @@ class StoryViewController: UIViewController {
     @IBOutlet var button3: UIButton!
     @IBOutlet var button4: UIButton!
     
+    var currentIndex = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        loadPage()
+    }
+    
+    func loadPage() {
+        let currentPage = story[currentIndex]
+        storyText.text = currentPage.text
+        button1.setTitle(currentPage.button1Text, for: .normal)
+        button2.setTitle(currentPage.button2Text, for: .normal)
+        button3.setTitle(currentPage.button3Text, for: .normal)
+        button4.setTitle(currentPage.button4Text, for: .normal)
+    }
 
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        let currentPage = story[currentIndex]
+        
+        if sender == button1 {
+            currentIndex = currentPage.button1Action
+        } else if sender == button2 {
+            currentIndex = currentPage.button2Action
+        } else if sender == button3 {
+            currentIndex = currentPage.button3Action
+        } else if sender == button4 {
+            currentIndex = currentPage.button4Action
+        }
+        
+        loadPage()
     }
     
 
