ecad8c0dd459e4f38c21cb55e907d9a7e025339a
Stacey Moore <smoore424@gmail.com>
2019-09-02 12:15:30 -0700

    basic tableview for about screen built

---
+++ CYOA/ViewControllers/Privacy.swift
@@ -0,0 +1,13 @@
+//
+//  Privacy.swift
+//  CYOA
+//
+//  Created by Stacey on 8/31/19.
+//  Copyright © 2019 Stacey. All rights reserved.
+//
+
+import Foundation
+
+class Privacy: UIViewController {
+    
+}
